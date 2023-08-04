9c7165547e89e029964d3a6f61914e18dee4f2d6
Rakib <rakib@Rakibs-MacBook-Pro.local>
2023-08-04 23:22:31 +0600

    sixth-project-starting

--- fifth_project/first_app/urls.py
+++ fifth_project/first_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.home,name ="homepage"),
     path('about/',views.about,name ="aboutpage"),
     path('submit_form/',views.submit_form,name ="submit_form"),
+    path('django_form/',views.django_form,name ="django_form"),
    
 ]
--- fifth_project/first_app/views.py
+++ fifth_project/first_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 # from django.shortcuts import render
+from . forms import contactForm
 
 def home(request):
     return render(request,'./first_app_temp/home.html',{"name" :"i am rahim","marks":86,
@@ -40,4 +41,19 @@ def about(request):
 
 def submit_form(request):
     return render (request,'./first_app_temp/form.html')
+
+def django_form(request):
+    if request.method =='POST':
+       form = contactForm(request.POST,request.FILES)
+       if form.is_valid():
+        #    file = form.cleaned_data('form')
+        #    with_open('./first_app/upload/',+ file.name ,'wb+') as destination:
+        #         for chunk in file.chunk():
+        #             destination.write(chunk)
+            print(form.cleaned_data)
+            return render(request,'./first_app_temp/django_form.html', {'form' : form})
+    else:
+        form = contactForm()
+    return render(request,'./first_app_temp/django_form.html', {'form' : form})
     
+        
