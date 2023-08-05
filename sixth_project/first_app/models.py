d17f1613f725a9789a8ba81173566d54cd02c19b
Rakib <rakib@Rakibs-MacBook-Pro.local>
2023-08-05 23:42:57 +0600

    Showing Model data to frontend

--- sixth_project/first_app/models.py
+++ sixth_project/first_app/models.py
@@ -5,5 +5,9 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=20)
     roll = models.IntegerField(primary_key=True)
-    address = models.TextField()
     fathers_name = models.TextField(default='Rakib')
+    address = models.TextField()
+    
+    def __str__(self):
+        return f"Roll:{self.roll} name: {self.name}"
+
