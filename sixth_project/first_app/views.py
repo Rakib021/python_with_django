804c611a0eb30a457a8152345d59b783a2bea8dd
Rakib <rakib@Rakibs-MacBook-Pro.local>
2023-08-06 16:13:15 +0600

    deleting-operation

--- sixth_project/first_app/views.py
+++ sixth_project/first_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 from . import models
 
 # Create your views here.
@@ -6,3 +6,12 @@ def home(request):
     student = models.Student.objects.all()
     # print(student)
     return render(request,'home.html',{'data':student})
+
+def delete_student(request,roll):
+    std = models.Student.objects.get(pk=roll).delete()
+    student = models.Student.objects.all()
+    # return redirect("homepage")
+    print(std)
+    return render(request,'home.html',{'data':student})
+    
+    
